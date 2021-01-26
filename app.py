c0bca6bdf84106413f696dc0449c98d4cecfbd88
Samuil Dimitrov <sami.dim03@gmail.com>
2021-01-26 23:24:06 +0200

    Topic

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ from flask_login import login_user, login_required, current_user, logout_user
 from flask_login import LoginManager
 from flask_sqlalchemy import SQLAlchemy
 from werkzeug.security import generate_password_hash, check_password_hash
+from datetime import datetime
 
 
 login_manager = LoginManager()
@@ -50,6 +51,7 @@ class User(db.Model):
 class Topic(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(80), unique=True, nullable=False)
+    description = db.Column(db.String(1000), unique=True, nullable=False)
 
 
 class Post(db.Model):
@@ -57,6 +59,7 @@ class Post(db.Model):
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
     topic_id = db.Column(db.Integer, db.ForeignKey('topic.id'), nullable=False)
     content = db.Column(db.String(10000), nullable=False)
+    date_created = db.Column(db.DateTime, default=datetime.utcnow)
 
 
 @app.route('/register', methods=['GET', 'POST'])
@@ -99,7 +102,27 @@ def login():
             flash("Wrong username or password!","danger")
     return response
 
-@app.route("/logout")
+@app.route('/create_topic', methods=['GET', 'POST'])
+@login_required
+def create_topic():
+    if request.method == "POST":
+        name = request.form["name"]
+        description = request.form["description"]
+        print(description);
+        topics = Topic.query.filter_by(name=name).first()
+
+        if topics:
+            flash("This topic already exists!","danger")
+            return render_template("create_topic.html")
+        else:
+            topic = Topic(name=name, description = description)
+            db.session.add(topic)
+            db.session.commit()
+            flash("Topic added successfully!","success")
+            return redirect(url_for('index'))
+    return render_template("create_topic.html")
+
+@app.route('/logout')
 @login_required
 def logout():
     current_user.login_id = None
@@ -113,6 +136,9 @@ def logout():
 def profile():
     return render_template("profile.html")
 
-@app.route('/')
+@app.route('/',methods=['GET', 'POST'])
 def index():
-    return render_template("index.html")
+    if request.method == "POST":
+        return redirect(url_for('create_topic'))
+    topics = Topic.query.order_by(Topic.id).all()
+    return render_template("index.html",topics = topics)
